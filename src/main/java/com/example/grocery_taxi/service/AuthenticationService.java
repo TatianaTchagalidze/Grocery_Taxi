7772ee3fb7cddbadb864626be6ed54e79b4be617
TatianaTchagalidze <tatiana.tchagalidze@gmail.com>
2023-06-21 04:08:44 +0400

    Using Lombok's @RequiredArgsConstructor for consistency

--- src/main/java/com/example/grocery_taxi/service/AuthenticationService.java
+++ src/main/java/com/example/grocery_taxi/service/AuthenticationService.java
@@ -2,21 +2,18 @@ package com.example.grocery_taxi.service;
 
 import com.example.grocery_taxi.entity.User;
 import com.example.grocery_taxi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
 
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
-
-  public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-    this.userRepository = userRepository;
-    this.passwordEncoder = passwordEncoder;
-  }
 
   public boolean authenticateUser(String email, String password) {
     Optional<User> optionalUser = userRepository.findByEmail(email);
