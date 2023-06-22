ed774fe4ce776054640d4de93a032d460593317e
TatianaTchagalidze <tatiana.tchagalidze@gmail.com>
2023-06-22 19:23:06 +0400

    adding JwtAuthenticationFilter

--- src/main/java/com/example/grocery_taxi/config/SecurityConfig.java
+++ src/main/java/com/example/grocery_taxi/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.example.grocery_taxi.config;
 
+import com.example.grocery_taxi.filter.JwtAuthenticationFilter;
 import com.example.grocery_taxi.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -16,10 +21,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final CustomUserDetailsService userDetailsService;
   private final JwtAuthenticationEntryPoint authenticationEntryPoint;
+  private final JwtUtil jwtUtil;
 
-  public SecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthenticationEntryPoint authenticationEntryPoint) {
+  public SecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthenticationEntryPoint authenticationEntryPoint, JwtUtil jwtUtil) {
     this.userDetailsService = userDetailsService;
     this.authenticationEntryPoint = authenticationEntryPoint;
+    this.jwtUtil = jwtUtil;
   }
 
   @Override
@@ -28,15 +35,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         .antMatchers("/users").permitAll()
         .antMatchers("/health").permitAll()
-        .anyRequest().permitAll()
+        .antMatchers("/login").permitAll()
+        .anyRequest().authenticated()
         .and()
-        .httpBasic()
-        .authenticationEntryPoint(authenticationEntryPoint);
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
   }
+
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+  }
+
+  @Bean
+  public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+    JwtAuthenticationFilter filter = new JwtAuthenticationFilter(authenticationManager(), jwtUtil);
+    return filter;
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
   }
 
   @Bean
